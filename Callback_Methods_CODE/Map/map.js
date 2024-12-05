13bc0b1b823f2f7d43577a3eb6e5173febb63acc
pujaPatil169 <marssitara6292@gmail.com>
2024-12-05 14:05:17 +0530

    adding code files

---
+++ Callback_Methods_CODE/Map/map.js
@@ -0,0 +1,19 @@
+const movies = [
+    {
+        title: 'Amadeus',
+        score: 99
+    },
+    {
+        title: 'Stand By Me',
+        score: 85
+    },
+    {
+        title: 'Parasite',
+        score: 95
+    },
+    {
+        title: 'Alien',
+        score: 90
+    }
+]
+const newmovies=movies.map((movie)=> `${movie.title.toUpperCase()}  - ${movie.score}`)
